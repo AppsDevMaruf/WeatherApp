f6c2eabbc7a7d334562b3bca9f57ecd3c8ab6075
Maruf Alam <44240762+AppsDevMaruf@users.noreply.github.com>
2022-10-10 10:21:03 +0600

    search add

--- app/src/main/java/com/marufalam/weatherapps/WeatherFragment.kt
+++ app/src/main/java/com/marufalam/weatherapps/WeatherFragment.kt
@@ -1,22 +1,70 @@
 package com.marufalam.weatherapps
 
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.marufalam.weatherapps.adapter.ForecastAdapter
 import com.marufalam.weatherapps.databinding.FragmentWeatherBinding
-import com.marufalam.weatherapps.models.CurrentModel
+
 import com.marufalam.weatherapps.viewmodels.LocationViewModel
 
 class WeatherFragment : Fragment() {
+    private val TAG = "WeatherFragment"
     val locationViewModel: LocationViewModel by activityViewModels()
     private lateinit var binding: FragmentWeatherBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+       inflater.inflate(R.menu.weather_menu,menu)
+        val searchView = menu.findItem(R.id.item_search).actionView as SearchView
+        searchView.isSubmitButtonEnabled = true
+        searchView.queryHint = "Search City"
+        searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    convertCityToLatLong(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+               return true;
+            }
+
+        })
+
+    }
+
+    private fun convertCityToLatLong(query: String?) {
+        val geocoder = Geocoder(requireActivity())
+        val addressList = geocoder.getFromLocationName(query,1)
+        if (addressList.isNotEmpty()){
+            val lat = addressList[0].latitude
+            val lng = addressList[0].longitude
+            val location = Location("").apply {
+                latitude = lat
+                longitude = lng
+            }
+            locationViewModel.setNewLocations(location)
+        }else{
+            Toast.makeText(requireActivity(), "Invalid City Name", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,14 +80,14 @@ class WeatherFragment : Fragment() {
         binding.forecastRV.adapter = adapter
 
         locationViewModel.locationLiveData.observe(viewLifecycleOwner) {
+            Log.e(TAG, "${it.latitude} ${it.longitude}")
             locationViewModel.fetchData()
         }
         locationViewModel.currentModelLD.observe(viewLifecycleOwner) {
-            Log.e("TAG", "WeatherFragment: ,${it.main.temp}")
-            println("...........")
+            Log.e(TAG, "WeatherFragment: ,${it.main.temp}")
             binding.current =it
 
-                Toast.makeText(requireActivity(), "${it.main.temp}", Toast.LENGTH_LONG).show()
+
         }
         locationViewModel.forecastModelLD.observe(viewLifecycleOwner) {
             adapter.submitList(it.list)
